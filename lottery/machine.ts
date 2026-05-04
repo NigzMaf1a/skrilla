f20588756e5db49587cdf369d6a92923188a9f87
Nigel Khasiani <118476871+NigzMaf1a@users.noreply.github.com>
2026-05-04 23:10:56 +0300

    Update lottery machine rules comment

--- lottery/machine.ts
+++ lottery/machine.ts
@@ -1,4 +1,4 @@
-//Rules of the lottery machine 
+//Rules of the lottery 
 /*
     -- The machine has four displays that each show a number from 0-9.
     --Winning sequences are set by the management
